61ef5fd505ed1eed7de469a25dbf44bf34811d76
eduardo01322 <eduardo.errea32@gmail.com>
2024-04-02 14:49:35 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Cardapio from "./src/cardapio";
 import Carrinho from "./src/carrinho";
+import CadastroProduto from "./src/screens/CadastroProduto";
 
 function App(): React.JSX.Element {
 
   return (
     //<Cardapio/>
-    <Carrinho/>
+    //<Carrinho/>
+    <CadastroProduto/>
   );
 }
 
